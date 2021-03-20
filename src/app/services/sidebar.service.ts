4e406d021d03a7b569e085257b38f3adfcf55e81
CSuncion <csuncion91@outlook.com>
2021-03-20 13:03:18 -0500

    Fin sección 7

---
+++ src/app/services/sidebar.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SidebarService {
+
+  menu: any[] = [
+    {
+      title: 'Dashboard',
+      ico: 'mdi mdi-gauge',
+      submenu: [
+        {
+          title: 'Main',
+          url: '/'
+        },
+        {
+          title: 'ProgressBar',
+          url: 'progress'
+        },
+        {
+          title: 'Gráficas',
+          url: 'graph1'
+        }
+
+      ]
+    }
+  ];
+
+
+  constructor() { }
+
+
+}
--- src/app/shared/siderbar/siderbar.component.ts
+++ src/app/shared/siderbar/siderbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SidebarService } from '../../services/sidebar.service';
 
 @Component({
   selector: 'app-siderbar',
@@ -8,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SiderbarComponent implements OnInit {
 
-  constructor() { }
+  menuItems: any[];
+
+  constructor(private sidebarServices: SidebarService) { 
+    this.menuItems = this.sidebarServices.menu;
+  }
 
   ngOnInit(): void {
   }
